7d744611d20170bd502cca63a250d4173164f9dd
Odin Dutton <odindutton@gmail.com>
2019-03-28 21:48:02 +1100

    Move all code into main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use diff_color;
+//! Color `diff --unified` output.
+//!
+//! # Example
+//!
+//! ```bash
+//! $ diff --unified <(git show 2e60019:README.md) README.md | diff-color
+//! ```
+use colored::Colorize;
+use std::io::{stdin, BufRead};
 use std::process;
 
 fn main() {
-    diff_color::run().unwrap_or_else(|err| {
-        println!("An error occured: {}", err);
-        process::exit(1);
-    });
+    for line in stdin().lock().lines() {
+        let line = line.unwrap_or_else(|err| {
+            println!("An error occured: {}", err);
+            process::exit(1);
+        });
+
+        if line.starts_with("---") {
+            println!("{}", line.yellow());
+        } else if line.starts_with("+++") {
+            println!("{}", line.yellow());
+        } else if line.starts_with("@@ ") {
+            println!("{}", line.cyan());
+        } else if line.starts_with("+") {
+            println!("{}", line.green());
+        } else if line.starts_with("-") {
+            println!("{}", line.red());
+        } else {
+            println!("{}", line);
+        }
+    }
 }
